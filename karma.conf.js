36db7549d2daf37ae195e617b187851ae9803a2f
Drew Keller <drew@wimpyprogrammer.com>
2017-09-24 00:53:31 -0500

    Install Karma test runner.
    Use webpack to bundle the app for browser testing.

---
+++ karma.conf.js
@@ -0,0 +1,31 @@
+module.exports = (config) => {
+	config.set({
+		frameworks: ['mocha'],
+
+		files: [
+			'src/**/*.spec.js',
+		],
+
+		preprocessors: {
+			'src/**/*.spec.js': ['webpack'],
+		},
+
+		webpack: {
+
+			externals: {
+				'react/lib/ExecutionEnvironment': true,
+				'react/addons': true,
+				'react/lib/ReactContext': 'window',
+			},
+
+			module: {
+				loaders: [
+					{ test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
+				],
+			},
+
+			devtool: 'eval-source-map',
+
+		},
+	});
+};
