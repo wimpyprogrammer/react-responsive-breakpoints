ce70e7eeec5659947df1c9afb3b9a04a499c68e9
Drew Keller <drew@wimpyprogrammer.com>
2017-10-07 21:13:30 -0500

    Run CI tests with Sauce Labs

---
+++ karma-ci.conf.js
@@ -0,0 +1,28 @@
+/* eslint import/no-extraneous-dependencies: ["error", {"devDependencies": true}] */
+const merge = require('lodash.merge');
+const commonConfig = require('./karma-common.conf');
+
+const browsersToTest = {
+	sl_win10_chrome61: {
+		base: 'SauceLabs',
+		browserName: 'chrome',
+		platform: 'Windows 10',
+		version: '61.0',
+	},
+};
+
+const ciConfig = merge(commonConfig, {
+	reporters: ['saucelabs'],
+
+	sauceLabs: {
+		testName: 'Library Karma Tests',
+	},
+
+	customLaunchers: browsersToTest,
+
+	browsers: Object.keys(browsersToTest),
+
+	singleRun: true,
+});
+
+module.exports = config => config.set(ciConfig);
