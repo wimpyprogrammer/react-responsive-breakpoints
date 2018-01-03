3498e6c645330f9194ca6078e7ca26288690d363
Drew Keller <drew@wimpyprogrammer.com>
2018-01-02 21:58:37 -0600

    Watch and poll for test changes
    Use polling for watch compatibility on Vagrant and Docker machines.

--- karma-local.conf.babel.js
+++ karma-local.conf.babel.js
@@ -1,5 +1,16 @@
+/* eslint import/no-extraneous-dependencies: ["error", {"devDependencies": true}] */
+import merge from 'lodash.merge';
 import commonConfig from './karma-common.conf';
 
-const localConfig = commonConfig;
+const localConfig = merge({
+	autoWatch: true,
+
+	webpackMiddleware: {
+		watchOptions: {
+			ignored: /node_modules/,
+			poll: 500,
+		},
+	},
+}, commonConfig);
 
 export default config => config.set(localConfig);
